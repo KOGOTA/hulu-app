1f15bc0df59641ff5b5a9542fcec4f61b5959964
unknown <ingeregere@gmail.com>
2022-05-31 20:48:32 +0200

    hulu app

---
+++ utils/requests.js
@@ -0,0 +1,57 @@
+const API_KEY = process.env.API_KEY;
+const requests ={
+    fetchTrending:{
+        title: 'Trending',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending1:{
+        title: 'Trending1',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending2:{
+        title: 'Trending2',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending3:{
+        title: 'Trending3',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending4:{
+        title: 'Trending4',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending5:{
+        title: 'Trending5',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending6:{
+        title: 'Trending6',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending7:{
+        title: 'Trending7',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending8:{
+        title: 'Trending8',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending9:{
+        title: 'Trending9',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending10:{
+        title: 'Trending10',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending11:{
+        title: 'Trending11',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTrending12:{
+        title: 'Trending12',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+}
+
+export default requests
